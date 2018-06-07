b6c9873958df3d1d5f6d869646fae14a63b8d1c3
Teschel6 <elijah.tesch@mail.wlc.edu>
2018-06-07 17:02:05 -0500

    started menu

--- assetManager/manager/tests.py
+++ assetManager/manager/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+import time
+import random
+from .models import *
+
+#Inventory Tests
+class InventoryModelTests(TestCase):
+    
+    def test_search_speed_large_inventory(self):
+        today = datetime.date.today()
+        size = 1000
+        #Create test Inventory
+        for i in range(size):
+            m = Inventory(asset_tag=i)
+            m.save()
+        start = time.time()
+        m = Inventory.objects.get(pk = random.randint(0,size - 1))
+        end = time.time()
+        print('Database lookup took: ',end - start,'S')
